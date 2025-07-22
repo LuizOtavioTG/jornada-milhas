cb47eccbade066f3fc96397fdffb4db290af3787
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-22 19:07:19 -0300

    Feat: Adicionando component form de buscar passagens.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Banner } from "./banner/banner";
+import { FormBuscaPassagens } from "./form-busca-passagens/form-busca-passagens";
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Banner],
+  imports: [Header, Banner, FormBuscaPassagens],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

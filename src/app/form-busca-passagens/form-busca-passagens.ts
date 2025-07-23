c4f5feb898a4d7d9cac4b081d1061892f870b8ae
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-22 21:55:51 -0300

    Feat: Adicionando campos input de origem e destino.

--- src/app/form-busca-passagens/form-busca-passagens.ts
+++ src/app/form-busca-passagens/form-busca-passagens.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @Component({
   selector: 'app-form-busca-passagens',
-  imports: [MatButtonToggleModule],
+  imports: [
+    MatButtonToggleModule,
+    MatInputModule,
+    MatFormFieldModule, 
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   templateUrl: './form-busca-passagens.html',
   styleUrl: './form-busca-passagens.scss'
 })
